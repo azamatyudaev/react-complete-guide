d55cc51e772d6ff24285754e82b47a43178e87f8
Azamat <azamatyudaev@gmail.com>
2022-12-04 00:59:24 +0500

    Lesson 1 First custom component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpensesItem from './components/ExpensesItem'
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpensesItem></ExpensesItem>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ExpensesItem.jsx
@@ -0,0 +1,7 @@
+const ExpensesItem = () => {
+  return (
+    <h2>Expense Item!</h2>
+  )
+}
+
+export default ExpensesItem
