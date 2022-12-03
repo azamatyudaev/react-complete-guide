8a983f6eb98d7274a6d58b2398e06669539e29b4
Azamat <azamatyudaev@gmail.com>
2022-12-04 01:04:40 +0500

    Lesson 2 More complex JSX code

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,6 +1,12 @@
 const ExpensesItem = () => {
   return (
-    <h2>Expense Item!</h2>
+    <div>
+      <div>December 4th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
   )
 }
 
