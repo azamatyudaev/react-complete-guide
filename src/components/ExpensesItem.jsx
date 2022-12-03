cf6a3e68658292cb73d8e2ad8239b6bdbdc237db
Azamat <azamatyudaev@gmail.com>
2022-12-04 01:31:05 +0500

    Lesson 5 Passing data via props

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,16 +1,16 @@
 import './ExpensesItem.css'
 
-const ExpensesItem = () => {
+const ExpensesItem = (props) => {
   const expenseDate = new Date(2021, 2, 28)
   const expenseTitle = 'Car Insurance'
   const expenseAmount = 294.67
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toLocaleDateString()}</div>
+      <div>{props.date.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   )
