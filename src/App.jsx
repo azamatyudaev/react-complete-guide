ed2eac9113f6ccda2b335d5083f8f634ef14b619
Azamat <azamatyudaev@gmail.com>
2022-12-04 17:20:56 +0500

    Lesson 7 Splitting components into multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14)
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -30,17 +35,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpensesItem>
+      />
       <ExpensesItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpensesItem>
+      />
       <ExpensesItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpensesItem>
+      />
+      <ExpensesItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   )
 }
--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,17 +1,11 @@
 import './ExpensesItem.css'
+import ExpensesDate from './ExpensesDate'
 
 const ExpensesItem = (props) => {
-  const month = props.date.toLocaleString('en-US', { month: 'long'})
-  const year = props.date.getFullYear()
-  const day = props.date.toLocaleString('en-US', { day: '2-digit'})
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpensesDate date={props.date} />
 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
