9a6d53d61639c20b32baa4a313950eaeaff64dca
Azamat <azamatyudaev@gmail.com>
2022-12-05 02:23:08 +0500

    Lesson 11 Event handlers

--- src/components/Expenses/ExpensesItem/ExpensesItem.jsx
+++ src/components/Expenses/ExpensesItem/ExpensesItem.jsx
@@ -3,6 +3,9 @@ import ExpensesDate from './ExpensesDate/ExpensesDate'
 import Card from '../../UI/Card'
 
 const ExpensesItem = (props) => {
+  const handleClick = () => {
+    console.log('Clicked!')
+  }
 
   return (
     <Card className="expense-item">
@@ -12,6 +15,7 @@ const ExpensesItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button type="button" onClick={handleClick}>Change Title</button>
     </Card>
   )
 }
