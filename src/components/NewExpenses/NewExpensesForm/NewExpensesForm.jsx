e67e35f7e105a430b7f0cd05ce20e9510b7a6f6a
Azamat <azamatyudaev@gmail.com>
2022-12-06 00:07:14 +0500

    Lesson 17 Using one state instead and what is better

--- src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
+++ src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
@@ -2,27 +2,43 @@ import './NewExpensesForm.css'
 import { useState } from 'react'
 
 const NewExpensesForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('')
-  const [enteredAmount, setEnteredAmount] = useState('')
-  const [enteredDate, setEnteredDate] = useState('')
+  // const [enteredTitle, setEnteredTitle] = useState('')
+  // const [enteredAmount, setEnteredAmount] = useState('')
+  // const [enteredDate, setEnteredDate] = useState('')
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  })
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value)
+    // setEnteredTitle(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value
+    })
   }
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value)
+    // setEnteredAmount(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value
+    })
   }
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value)
+    // setEnteredDate(event.target.value)
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value
+    })
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(enteredTitle)
-    console.log(enteredAmount)
-    console.log(enteredDate)
+    console.log(userInput)
   }
 
   return (
