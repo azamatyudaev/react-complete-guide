e9a48c946bee2832fd98c99320b542797a426c06
Azamat <azamatyudaev@gmail.com>
2022-12-05 18:58:33 +0500

    Lesson 15 Listening to user input

--- src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
+++ src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
@@ -1,12 +1,18 @@
 import './NewExpensesForm.css'
 
 const NewExpensesForm = () => {
+  const handleChange = (event) => {
+    console.log('Title changed!')
+    console.log( event)
+    console.log(event.target.value)
+  }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" />
+          <input type="text" id="title" onChange={handleChange} />
         </div>
 
         <div className="new-expense__control">
