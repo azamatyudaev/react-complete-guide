83b91eea568caeba5ed8930f2ca790a976b998ad
Azamat <azamatyudaev@gmail.com>
2022-12-05 21:00:40 +0500

    Lesson 16 Working with multiple states

--- src/components/Expenses/ExpensesItem/ExpensesItem.jsx
+++ src/components/Expenses/ExpensesItem/ExpensesItem.jsx
@@ -1,26 +1,18 @@
 import './ExpensesItem.css'
-import { useState } from 'react'
 import ExpensesDate from './ExpensesDate/ExpensesDate'
 import Card from '../../UI/Card'
 
 
 const ExpensesItem = (props) => {
-  const [title, setTitle] = useState(props.title)
-
-  const handleClick = () => {
-    setTitle('Updated!')
-    console.log(title) // it shows old value because useState update function does not change actual value
-  }
 
   return (
     <Card className="expense-item">
       <ExpensesDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button type="button" onClick={handleClick}>Change Title</button>
     </Card>
   )
 }
--- src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
+++ src/components/NewExpenses/NewExpensesForm/NewExpensesForm.jsx
@@ -1,28 +1,58 @@
 import './NewExpensesForm.css'
+import { useState } from 'react'
 
 const NewExpensesForm = () => {
-  const handleChange = (event) => {
-    console.log('Title changed!')
-    console.log( event)
-    console.log(event.target.value)
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value)
+  }
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(enteredTitle)
+    console.log(enteredAmount)
+    console.log(enteredDate)
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" onChange={handleChange} />
+          <input type="text" id="title" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            id="amount"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
 
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" min="2022-01-01" max="2032-12-31" />
+          <input
+            type="date"
+            id="date"
+            onChange={dateChangeHandler}
+            min="2022-01-01"
+            max="2032-12-31"
+          />
         </div>
       </div>
 
