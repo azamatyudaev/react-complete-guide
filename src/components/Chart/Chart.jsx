09a625f1e0e4d2f05adbcf703e7cd241faf5e69c
azamatyudaev <azamatyudaev@gmail.com>
2022-12-07 15:01:32 +0500

    Lesson 30 Adding Chart

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,19 @@
+import './Chart.css'
+import ChartBar from './ChartBar/ChartBar'
+
+const Chart = ({ charts }) => {
+  return (
+    <div className="chart">
+      {charts.map((chart) => (
+        <ChartBar
+          key={chart.id}
+          value={chart.value}
+          maxValue={null}
+          label={chart.label}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Chart
---
+++ src/components/Chart/ChartBar/ChartBar.jsx
@@ -0,0 +1,11 @@
+import './ChartBar.css'
+
+const ChartBar = ({ value, maxValue, label }) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ChartBar
